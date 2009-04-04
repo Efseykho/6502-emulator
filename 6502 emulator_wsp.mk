c1d1d5fab9dc2bf12676ac2a9e3f7a4e84895200
Efseykho <mturovskiy@gmail.com>
2009-04-03 22:57:37 -0400

    Remove files for first refactor!

--- 6502 emulator_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ 6502 - Debug ]----------
-	@cd "6502" && "mingw32-make.exe"  -j 1 -f "6502.mk" 
-clean:
-	@echo ----------Cleaning project:[ 6502 - Debug ]----------
-	@cd "6502" && "mingw32-make.exe"  -j 1 -f "6502.mk" clean
