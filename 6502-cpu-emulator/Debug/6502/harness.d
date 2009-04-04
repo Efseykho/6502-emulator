44ebac7eb857649e1f84bc62a4a3f6c4bcaa8f7a
Efseykho <mturovskiy@gmail.com>
2009-04-03 23:37:42 -0400

    Remove *.exe/*.o from repo

--- 6502-cpu-emulator/Debug/6502/harness.d
+++
@@ -1,8 +0,0 @@
-6502/harness.d 6502/harness.o: ../6502/harness.c ../6502/em_6502.h \
-  ../6502/definitions.h ../6502/unit_test.h
-
-../6502/em_6502.h:
-
-../6502/definitions.h:
-
-../6502/unit_test.h:
